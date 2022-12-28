2aeed389a41f89862c6b151958425fca5559051a
ceavinr <16521304@std.stei.itb.ac.id>
2022-12-28 14:11:27 +0700

    upload image

---
+++ backend/src/Models.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const pesertaSchema = new Schema({
+  email: String,
+  nama: String,
+  ttl: String,
+  gender: String,
+  sekolah: String,
+  lomba: String,
+  idline: String,
+  notelp: String,
+  kartu_identitas: String,
+  surat_keterangan: String,
+  bukti_pembayaran: String,
+  is_verified: Boolean,
+});
+const peserta = mongoose.model("peserta", pesertaSchema);
+
+module.exports = {
+  peserta,
+};
