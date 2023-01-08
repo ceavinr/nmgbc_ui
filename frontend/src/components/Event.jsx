07cb60fc0bcc992d335328b3b6929eddf87234fe
ceavinrufus <18221162@std.stei.itb.ac.id>
2023-01-08 22:07:39 +0700

    tulisan rata kanan

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -5,9 +5,9 @@ function Event(props) {
   return (
     <div className={props.className}>
       <div className="bg-[#FFF3BF] px-6 py-1 md:py-2">
-        <div className="flex justify-between md:justify-end items-center gap-2">
+        <div className="flex justify-end items-center gap-2">
           <div className="font-Volkswagen space-y-2">
-            <h1 className="pr-[55px] md:text-right md:text-xl xl:text-2xl">
+            <h1 className="pr-[40px] md:pr-[55px] text-right md:text-xl xl:text-2xl">
               {props.children}
             </h1>
             <HashLink
@@ -15,7 +15,7 @@ function Event(props) {
               to={props.link}
               className="underline text-sm md:text-base xl:text-xl md:text-right"
             >
-              <div className="md:text-right">learn more</div>
+              <div className="text-right">learn more</div>
             </HashLink>
           </div>
           <div className="flex items-center">
