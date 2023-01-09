6c4211670a321b8fccccb3db6ed53c0abc56571c
ceavinrufus <18221162@std.stei.itb.ac.id>
2023-01-09 07:24:08 +0700

    cors-config

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,8 @@ var cors = require("cors");
 app.use(
   cors({
     origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+    credentials: true,
+    optionsSuccessStatus: 200,
   })
 );
 
