e61bc185b7e8980e21ba753a4a796d17c50dcae6
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-01-20 21:56:38 +0700

    copyright

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,8 +3,16 @@ import React from "react";
 function Footer() {
   return (
     <div className="bg-[#B59DED] px-6 md:px-12 py-2 text-white font-Stanberry text-xs md:text-sm lg:text-base">
-      Contact Person: Azka Nadia (081295254535 - ID Line: aazkanadia) | Rasheesa
-      Maharani (ID Line: rasha.hediarto)
+      <div className="">
+        Contact Person: Azka Nadia (081295254535 - ID Line: aazkanadia) |
+        Rasheesa Maharani (ID Line: rasha.hediarto)
+      </div>
+      <div className="text-sm w-full flex justify-end">
+        Built by&nbsp;
+        <a href="https://ceavinrufus.netlify.app/" className="underline">
+          Ceavin Rufus
+        </a>
+      </div>
     </div>
   );
 }
